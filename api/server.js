a8ea44ad436d41c6fc7acaa2a57d4547f0d8d956
Alex Edwards <alexedwards@Alexs-MacBook-Pro.local>
2021-08-04 19:27:10 -0600

    i hate coding

--- api/server.js
+++ api/server.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const usersRouter = require('./users/users-router.js');
 
 const server = express();
 
 // remember express by default cannot parse JSON in request bodies
-
+server.use(express.json())
 // global middlewares and the user's router need to be connected here
+
+server.use('/api/users', (req, res, next) => {next()}, usersRouter)
+
+
+server.use('*', (req, res, next) => {
+  console.log(`hitting ${req.method} ${req.baseUrl}`);
+  next({ status: 404, message: 'not found' });
+});
+
+server.use((err, req, res, next) => { 
+  res.status(err.status || 500).json({ message: `HORROR: ${err.message}` });
+});
+
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,41 +1,86 @@
-const express = require('express');
+const express = require("express");
 
 // You will need `users-model.js` and `posts-model.js` both
+const Users = require("./users-model.js");
+const Posts = require("../posts/posts-model");
 // The middleware functions also need to be required
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+function checkIdExists(req, res, next) {
+  const { id } = req.params
+  Users.findById(id)
+    .then(user => {
+      if (user) {
+        req.user = user 
+        next() 
+      } else {
+       
+        next({ message: `user with id ${id} not found!!!` })
+      }
+    })
+    .catch(next)
+}
+
+function checkUserPayload(req, res, next) {
+  // if req.body legit call next
+  // otherwise call next in a sadder way...
+  if (!req.body.name) { // req.body always exists, at least as {}
+    next({ message: `please provide a name!!!!`, status: 422 })
+  } else if (req.body.name < 3) {
+    next({ message: `name must be at least 3 chars`, status: 422 })
+  } else {
+    // happy happy
+    next()
+  }
+}
+
+router.get("/", (req, res, next) => {
   // RETURN AN ARRAY WITH ALL THE USERS
+  User.get()
+    .then((users) => {
+      res.status(200).json(users);
+    })
+    .catch((error) => {
+      next(error); // send that object over to the err handling midd!
+    });
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", checkIdExists, (req, res, next) => {
   // RETURN THE USER OBJECT
   // this needs a middleware to verify user id
+  res.json(req.hub)
 });
 
-router.post('/', (req, res) => {
+router.post("/", checkUserPayload, (req, res, next) => {
   // RETURN THE NEWLY CREATED USER OBJECT
   // this needs a middleware to check that the request body is valid
+  Users.add(req.body)
+    .then(user => {
+      res.status(201).json(user);
+    })
+    .catch(error => {
+      next(error)
+    });
 });
 
-router.put('/:id', (req, res) => {
-  // RETURN THE FRESHLY UPDATED USER OBJECT
+router.put("/:id", (req, res, next) => {
+  // RETURN THE F res, FRESHHLY UPDATED USER OBJECT
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
 });
 
-router.delete('/:id', (req, res) => {
-  // RETURN THE FRESHLY DELETED USER OBJECT
+router.delete("/:id", (req, res, next) => {
+  // RETURN THE F res, FRESHLY DELETED USER OBJECT
   // this needs a middleware to verify user id
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get("/:id/posts", (req, res, next) => {
   // RETURN THE ARRAY OF USER POSTS
   // this needs a middleware to verify user id
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post("/:id/posts", (req, res, next) => {
   // RETURN THE NEWLY CREATED USER POST
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
